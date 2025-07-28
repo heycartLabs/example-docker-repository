a1249443c66a8de394a367a7bbd5061f3d0e22d4
gxs <lyricsong47@gmail.com>
2025-07-28 23:40:29 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+#syntax=docker/dockerfile:1.4
+
+ARG BASE_IMAGE=heycart/docker-base:8.3
+
+# pin versions
+FROM ${BASE_IMAGE} as base-image
+FROM ghcr.io/friendsofheycart/heycart-cli:latest-php-8.3 as heycart-cli
+
+# build
+
+FROM heycart-cli as build
+
+ADD . /src
+WORKDIR /src
+
+RUN /usr/local/bin/entrypoint.sh heycart-cli project ci /src
+
+# build final image
+
+FROM base-image
+
+COPY --from=build --chown=82 --link /src /var/www/html
---
+++ config/bundles.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    HeyCart\Core\Framework\Framework::class => ['all' => true],
+    HeyCart\Core\System\System::class => ['all' => true],
+    HeyCart\Core\Content\Content::class => ['all' => true],
+    HeyCart\Core\Checkout\Checkout::class => ['all' => true],
+    HeyCart\Core\Maintenance\Maintenance::class => ['all' => true],
+    HeyCart\Core\DevOps\DevOps::class => ['e2e' => true],
+    HeyCart\Core\Profiling\Profiling::class => ['all' => true],
+    HeyCart\Administration\Administration::class => ['all' => true],
+    HeyCart\Elasticsearch\Elasticsearch::class => ['all' => true],
+    HeyCart\Storefront\Storefront::class => ['all' => true],
+];
